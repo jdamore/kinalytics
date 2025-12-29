b3015f04fcdc64f8ccc5a14ca3799b15a608be87
Jean D'Amore <jean.damore@gmail.com>
2025-12-29 22:51:43 +0100

    Upgraded kotlin 2.1.0 -> 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.1.0"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
 }
 
 group = "com.finalytics"
